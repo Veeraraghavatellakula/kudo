ece5cc6fdf549f9a2ac3952e6352168e14340ca4
veera raghava <veeratellakula@gmail.com>
2025-03-07 10:56:45 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import PlaceForm from './components/PlaceForm';
 import PlaceList from './components/PlaceList';
-import axios from 'axios';
 
 function App() {
   const [places, setPlaces] = useState([]);
@@ -10,18 +9,6 @@ function App() {
     // Load from localStorage on mount
     const storedPlaces = JSON.parse(localStorage.getItem('places')) || [];
     setPlaces(storedPlaces);
-    
-    // Fetch initial data from JSONPlaceholder
-    axios.get('https://jsonplaceholder.typicode.com/users')
-      .then(response => {
-        const initialData = response.data.slice(0, 3).map(user => ({
-          id: user.id,
-          name: user.address.city // Using city as place name
-        }));
-        setPlaces(initialData);
-        localStorage.setItem('places', JSON.stringify(initialData));
-      })
-      .catch(error => console.error('Error fetching data:', error));
   }, []);
 
   const addPlace = (name) => {
